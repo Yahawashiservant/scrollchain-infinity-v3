503e7e0ad635b6c636175241a456d21fa8ac4e21
Yahawashiservant <keithdwhitfield@gmail.com>
2025-04-01 19:41:50 -0400

    Create index.js

---
+++ api/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'monospace', padding: '2rem' }}>
+      <h1>ScrollChain Infinity v3</h1>
+      <p>Prophecy Portal is active.</p>
+    </div>
+  );
+}
